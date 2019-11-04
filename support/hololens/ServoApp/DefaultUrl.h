c48c922c9c01110c6f996064b103713ce10a2258
Manish Goregaokar <manishsmail@gmail.com>
2019-11-03 18:40:37 -0800

    fix url

--- support/hololens/ServoApp/DefaultUrl.h
+++ support/hololens/ServoApp/DefaultUrl.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define DEFAULT_URL L"https://manishearth.github.io/webgl-to-webxr/webxr-ar.html";
+#define DEFAULT_URL L"https://manishearth.net/sand/webxr-three/examples/webvr_ballshooter.html";
